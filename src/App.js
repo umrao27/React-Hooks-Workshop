e6abc0e9b9979d17fc4bcfa0a342dd3c302069b5
Rahul Umrao <rahul@Rahuls-MacBook-Pro.local>
2020-05-10 03:36:35 +0530

    code cleanup

--- src/App.js
+++ src/App.js
@@ -5,18 +5,15 @@ import UseStateExample from './Hooks/useState';
 import UseReducerExample from './Hooks/useReducer';
 import UseEffectExample from './Hooks/useEffect';
 import UseContextExample from './Hooks/useContext/parent';
-// import DashBoard from './Hooks/React-DashBoard';
-
 
 function App() {
   return (
     <div className="App">
-      {/* <DashBoard /> */}
       <UseStateExample />
       <hr />
-      <UseReducerExample />
-      <hr />
       <UseEffectExample />
+      <hr />
+      <UseReducerExample />
       <hr />
       <UseContextExample />
     </div>
